f5ed2d4f14a97634efad9245da7dde3c2c0402b6
varadan13 <rcaesar1996@gmail.com>
2025-11-11 19:21:30 +0530

    feat: add mini Kravein state definitions

--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
-const KraveinStateSchema = z.object({
+const kraveinStateSchema = z.object({
   allowedFoodOrderServiceTypeSettings: z
     .enum(["PICKUP", "DELIVERY", "DINEIN"])
     .describe(
@@ -63,3 +63,61 @@ const KraveinStateSchema = z.object({
     "PAYMENT_DETAILS",
   ]),
 });
+
+const miniKraveinStateSchema = z.object({
+  allowedFoodOrderServiceTypeSettings: z
+    .enum(["PICKUP", "DELIVERY", "DINEIN"])
+    .describe(
+      "List of supported service types where users can create a food order from kravein"
+    ),
+  menuTypes: z.enum(["NORMAL_MENU"]).describe(`
+        For each service type available to the user, there are multiple menus.
+        
+        Each menu contains multiple categories.
+        
+        Each category contains multiple items.
+    `),
+  categoryTypes: z.enum(["NORMAL_CATEGORY"]).describe(``),
+  itemTypes: z
+    .enum(["NORMAL_ITEM", "ITEM_WITH_VARIANT", "MULTISELECT_ITEM"])
+    .describe(
+      `List of supported food/item types that is available to the users for creating a cart`
+    ),
+  allowedSchedulingOptions: z
+    .enum(["ASAP", "SCHEDULED"])
+    .describe(
+      "List of supported scheduling options available to users when creating a food order from kravein"
+    ),
+  allowedPaymentOptions: z
+    .enum(["CARD", "GOOGLE_PAY", "APPLE_PAY", "CASH"])
+    .describe(
+      "List of supported payment options available to users when creating a food order from kravein"
+    ),
+  dealTypes: z
+    .enum(["AUTOMATIC_PROMOTION", "MANUAL_PROMOTION"])
+    .describe(
+      "List of supported deal types available to users when creating a food order from kravein"
+    ),
+  loginOptionsAvailable: z.enum([
+    "NORMAL_LOGIN",
+    "FEDERATED_LOGIN",
+    "GUEST_FLOW",
+    "NORMAL_SIGNUP",
+  ]),
+});
+
+const miniObj = {
+  allowedFoodOrderServiceTypeSettings: ["PICKUP", "DELIVERY", "DINEIN"],
+  menuTypes: ["NORMAL_MENU"],
+  categoryTypes: ["NORMAL_CATEGORY"],
+  itemTypes: ["NORMAL_ITEM_+_ITEM_WITH_VARIANT_+_MULTISELECT_ITEM"],
+  allowedSchedulingOptions: ["ASAP", "SCHEDULED"],
+  allowedPaymentOptions: ["CARD", "GOOGLE_PAY", "APPLE_PAY", "CASH"],
+  dealTypes: ["AUTOMATIC_PROMOTION", "MANUAL_PROMOTION"],
+  loginOptionsAvailable: [
+    "NORMAL_LOGIN",
+    "FEDERATED_LOGIN",
+    "GUEST_FLOW",
+    "NORMAL_SIGNUP",
+  ],
+};
